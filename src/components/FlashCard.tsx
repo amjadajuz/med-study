e0df46d8217f43ed460bd09b322cf9723e5f4876
amjadajuz <ptamjadaju@gmail.com>
2026-02-14 23:46:23 +0530

    Header UI adjustments

--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -1,3 +1,4 @@
+import { RotateCw } from 'lucide-react';
 import React, { useState } from 'react'
 
 type Props = {
@@ -22,16 +23,24 @@ const FlashCard = (props: Props) => {
         <h3 className="text-lg font-semibold mb-2">Tags</h3>
         <div className="flex flex-wrap gap-2">
           {props.tags.map((tag, index) => (
-            <span key={index} className="bg-accent text-primary px-2 py-1 rounded-full text-sm font-medium">
+            <span key={index} className="bg-accent text-foreground px-2 py-1 rounded-full text-sm font-medium">
               {tag}
             </span>
           ))}
         </div>
+        <div className="flex items-center justify-center gap-2 pt-4 border-t border-border/50 mt-4 text-[0.875rem] text-muted-foreground">
+              <RotateCw className="w-4 h-4" />
+              <span>Click to reveal answer</span>
+            </div>
       </div>
         </div>
         <div className="flip-card-back border-b bg-secondary text-primary rounded-lg">
           <h3 className="text-lg font-semibold mb-2">Answer:</h3>
           <p>{props.back}</p>
+            <div className="flex items-center justify-center gap-2 pt-4 border-t border-border/50 mt-4 text-[0.875rem] text-muted-foreground">
+              <RotateCw className="w-4 h-4" />
+              <span>Click to flip back</span>
+            </div>
         </div>
       </div>
       
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,10 +10,12 @@ type Props = {
 const Header = ({ toggleDarkMode, darkMode, scrollbarProgress }: Props) => {
   return (
     <header className="sticky top-0 w-full h-[50px] px-[20vw] bg-background text-foreground flex items-center border-b border-border">
-      <div className="flex justify-between w-full">
+      <div className="flex justify-between w-full py-2">
+        <h1 className="text-xl font-bold">
         Header
+        </h1>
         <button onClick={toggleDarkMode} className="cursor-pointer">
-          {!darkMode ? <Moon className="w-4 h-4" /> : <Sun className="w-4 h-4" />}
+          {!darkMode ? <Moon className="w-6  h-6" /> : <Sun className="w-6 h-6" />}
         </button>   
       </div>
       <div className="absolute bottom-0 left-0 h-[2px] bg-primary transition-all duration-150" style={{ width: `${scrollbarProgress}%` }}></div>
